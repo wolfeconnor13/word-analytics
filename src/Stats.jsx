1654c5006369b3bf4d8243ecdd24a77e507d2892
Cwolfe13 <wolfe.connor@gmail.com>
2025-06-01 16:58:44 -0400

    Added logic to give the stat number red text if going below the character limit.

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -17,7 +17,11 @@ export default function Stats({ text }) {
 function Stat({ label, number }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      <span
+        className={`stat__number ${number < 0 ? "stat__number--limit" : ""}`}
+      >
+        {number}
+      </span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
