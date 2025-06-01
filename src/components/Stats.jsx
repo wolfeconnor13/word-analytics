01a9528127b91263259070c33bc597825018a070
Cwolfe13 <wolfe.connor@gmail.com>
2025-06-01 17:04:17 -0400

    Improve structure by moving constants to a constants file. Clean up folder structure.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,13 @@
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constant";
+
 export default function Stats({ text }) {
   const numberOfCharacters = text.length;
   const numberOfWords = text.trim().split(/\s+/).length;
-  const instagramCharactersLeft = 280 - numberOfCharacters;
-  const facebookCharactersLeft = 2200 - numberOfCharacters;
+  const instagramCharactersLeft = INSTAGRAM_MAX_CHARACTERS - numberOfCharacters;
+  const facebookCharactersLeft = FACEBOOK_MAX_CHARACTERS - numberOfCharacters;
 
   return (
     <section className="stats">
